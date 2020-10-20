82df376d072e4bda66e5c4bf86cdce1ed86d9338
myari211 <apputra16@gmail.com>
2020-10-20 09:13:38 +0700

    change db

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$conn = new mysqli('localhost', 'h84845_root', 'Caleb26100@', 'h84845_hrmsystem');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
